b0d40ff7e021cbdca56a922e7f771b0327394c91
Maria Mills <cxwinterx@gmail.com>
2022-10-20 14:27:50 +0000

    -Corrections from Assigniment 2 -Added Accessibility -Utilized HTML Canvas on page 1 -Added iframes on page 2 -Added HTML SVG Graphic on page 3

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 //Array will not reset upon submition / page refresh however because it isn't locally stored or using an actual DB
 const usernames = ["Harry", "Elizabeth", "Shana"];
 
+//Getting the canvas by Class name using querySelector
+const canvas = document.querySelector('.canvas');
+// Use getContext to use the canvas for drawing
+const ctx = canvas.getContext("2d");
+
+
 //Function for form validation
 function validateForm() {
   const password = document.forms["form"]["password"].value;
@@ -23,4 +29,26 @@ function validateForm() {
     alert("Welcome");
     return true;
   }
+}
+
+//Function for drawing text using HTML Canvas
+function drawText() {
+  //Setting fill (color)
+  ctx.fillStyle = "#262261";
+  //Setting the font style for the text after
+  ctx.font = "Italic 30px Sans-Serif";
+
+  ctx.textBaseline = "middle";
+
+  ctx.fillText("Move aside!", 40, 100);
+
+  //Setting the font style for the text after
+  ctx.font = "Bold 24px Sans-Serif";
+  //Stroke text, makes text using outline
+  ctx.strokeText("Hello world!", 40, 50);
+}
+
+//If the browser supports canvas
+if (canvas.getContext) {
+  drawText();
 }
